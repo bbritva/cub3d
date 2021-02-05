ce53b8ee72ff94171537097237cc6f8fe2ed5fce
grvelva <grvelva@student.21-school.ru>
2021-02-05 20:21:42 +0300

    надо улучшать алгоритмы

--- Render/render.c
+++ Render/render.c
@@ -137,7 +137,7 @@ void 		draw_view(t_win *win)
 	i = 0;
 	while (i < win->prms->res_h)
 	{
-		angle = win->prms->plr->ang_h + M_PI_4 - i * (M_PI / (3 *
+		angle = win->prms->plr->ang_h + M_PI / 6 - i * (M_PI / (3 *
 				win->prms->res_h));
 		draw_line(win, i, get_height2(win, angle));
 		i += 1;
