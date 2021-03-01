a79345b15bbe2bfdfce42f1a752f3d0f3572a21c
grvelva <grvelva@student.21-school.ru>
2021-03-01 16:48:13 +0300

    исправил обработку аргументов программы

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: grvelva <grvelva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 18:59:31 by grvelva           #+#    #+#             */
-/*   Updated: 2021/02/27 11:48:17 by grvelva          ###   ########.fr       */
+/*   Updated: 2021/03/01 16:47:21 by grvelva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,13 @@ int		check_main_input(int argc, char *argv[])
 		ft_putstr(F_MISS_MSG);
 		return (0);
 	}
-	if (argc == 2 && ft_strnstr(argv[1], ".cub", ft_strlen(argv[1])))
+	if (argc == 2 && ft_strlen(ft_strnstr(argv[1], ".cub",
+		ft_strlen(argv[1]))) == 4)
 		return (1);
-	if (argc == 3 && ft_strncmp("––save", argv[2], 7))
-		if (ft_strlen(ft_strnstr(argv[1], ".cub", ft_strlen(argv[1]))) == 4)
-			return (1);
+	if (argc == 3 &&
+	ft_strlen(ft_strnstr(argv[2], "--save", ft_strlen(argv[1]))) == 6 &&
+	ft_strlen(ft_strnstr(argv[1], ".cub", ft_strlen(argv[1]))) == 4)
+		return (1);
 	ft_putstr(M_ARGS_MSG);
 	return (0);
 }
