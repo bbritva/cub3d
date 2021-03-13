37d1bcb7d831482959081c7345302719d72dae76
grvelva <grvelva@student.21-school.ru>
2021-03-13 13:26:52 +0300

    убрал затемнение из проекта без бонусов

--- render/drawers_utils.c
+++ render/drawers_utils.c
@@ -6,7 +6,7 @@
 /*   By: grvelva <grvelva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:00:04 by grvelva           #+#    #+#             */
-/*   Updated: 2021/03/07 10:17:27 by grvelva          ###   ########.fr       */
+/*   Updated: 2021/03/13 12:03:55 by grvelva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,6 @@ static int	get_pxl(t_tex *tex, int i, int h, int x_coor)
 	x = tex->w_tex * x_coor / 255;
 	src = tex->addr + (y * tex->line_l + x * (tex->bpp / 8));
 	color = *(int *)src;
-	return (color);
-}
-
-static int	shadow_color(int color, int h, int res_h)
-{
-	int red;
-	int green;
-	int blue;
-	int	base;
-
-	base = (int)((double)res_h / 8 / tan(M_PI / 6));
-	if (h > base)
-		return (color);
-	h = (h < base / 10) ? (base / 10) : h;
-	red = ((color & 0xFF << 16) >> 16) * h / base;
-	green = ((color & 0xFF << 8) * h >> 8) / base;
-	blue = (color & 0xFF) * h / base;
-	color = red << 16 | green << 8 | blue;
 	return (color);
 }
 
@@ -66,7 +48,7 @@ void		draw_txtr_line(t_all *all, int x_pos, int h)
 			break ;
 		if ((color = get_pxl(tex, i, h, x_coor)) > 0)
 			my_pixel_put(all->win, x_pos, (all->prms->res_v - h) / 2 + i,
-				shadow_color(color, h, all->prms->res_h));
+				color);
 		i++;
 	}
 }
