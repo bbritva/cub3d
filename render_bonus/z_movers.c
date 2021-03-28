aad0e35deb4766987b78c2a3e8be30686c1a2a62
grvelva <grvelva@student.21-school.ru>
2021-03-28 15:33:30 +0300

    научил зомбаков обходить друг друга и перешагивать через труп

--- render_bonus/z_movers.c
+++ render_bonus/z_movers.c
@@ -6,13 +6,25 @@
 /*   By: grvelva <grvelva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 09:51:18 by grvelva           #+#    #+#             */
-/*   Updated: 2021/03/28 13:57:18 by grvelva          ###   ########.fr       */
+/*   Updated: 2021/03/28 15:30:10 by grvelva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes_bonus/cub3d.h"
 
-static int		can_step(t_all *all, double i, double j)
+static int		is_z_near(t_all *all, double i, double j, int index)
+{
+	double dist;
+
+	if (index == 0)
+		return (0);
+	dist = pow(i - all->prms->sprites[index - 1]->pos_y, 2) +
+		pow(j - all->prms->sprites[index - 1]->pos_x, 2);
+	return (dist < pow(0.4, 2) &&
+		(all->prms->sprites[index - 1]->status_mask & Z_ALIVE));
+}
+
+static int		can_step(t_all *all, double i, double j, int index)
 {
 	if (all->prms->map[(int)i][(int)j] == '1')
 		return (0);
@@ -24,25 +36,24 @@ static int		can_step(t_all *all, double i, double j)
 		return (0);
 	if (all->prms->map[(int)i][(int)(j - D)] == '1')
 		return (0);
-	return (1);
+	return (!is_z_near(all, i, j, index));
 }
 
-static int		move_z(t_all *all, double ang, double *pos_x, double
-*pos_y)
+static int		move_z(t_all *all, double ang, int index)
 {
 	double i_cur;
 	double j_cur;
 	double i_new;
 	double j_new;
 
-	i_cur = *pos_y;
-	j_cur = *pos_x;
+	i_cur = all->prms->sprites[index]->pos_y;
+	j_cur = all->prms->sprites[index]->pos_x;
 	i_new = (i_cur - SPEED_Z * sin(ang));
 	j_new = (j_cur + SPEED_Z * cos(ang));
-	if (can_step(all, i_new, j_cur))
-		*pos_y -= SPEED_Z * sin(ang);
-	if (can_step(all, i_cur, j_new))
-		*pos_x += SPEED_Z * cos(ang);
+	if (can_step(all, i_new, j_cur, index))
+		all->prms->sprites[index]->pos_y -= SPEED_Z * sin(ang);
+	if (can_step(all, i_cur, j_new, index))
+		all->prms->sprites[index]->pos_x += SPEED_Z * cos(ang);
 	return (0);
 }
 
@@ -61,15 +72,14 @@ void			move_zombies(t_all *all)
 		if (all->prms->sprites[i]->dist > (0.5 + i * D * 3) &&
 				all->prms->sprites[i]->status_mask & Z_GOING &&
 				all->prms->sprites[i]->status_mask & Z_ALIVE)
-			move_z(all, all->prms->sprites[i]->angle + M_PI,
-				&all->prms->sprites[i]->pos_x, &all->prms->sprites[i]->pos_y);
+			move_z(all, all->prms->sprites[i]->angle + M_PI, i);
 		i++;
 	}
 	if (all->prms->sprites[0]->dist < 0.6 && count % 10 == 0 &&
 	all->prms->plr.health && all->prms->sprites[0]->status_mask & Z_ALIVE)
 	{
 		all->win->move_mask = all->win->move_mask | IS_BITTEN;
-		all->prms->plr.health -= (all->prms->plr.health > 0) ? 20 : 0;
+//		all->prms->plr.health -= (all->prms->plr.health > 0) ? 20 : 0;
 		system("afplay resources/sounds/bite.mp3 &");
 	}
 	count = (count > 10 || all->prms->sprites[0]->dist > 0.6) ? 0 : count;
