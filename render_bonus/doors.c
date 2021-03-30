05542fd86a81b450ac3d5428f6678b1f3bce7711
grvelva <grvelva@student.21-school.ru>
2021-03-30 13:07:35 +0300

    рисуется как надо, но сегается и пропадает

--- render_bonus/doors.c
+++ render_bonus/doors.c
@@ -6,7 +6,7 @@
 /*   By: grvelva <grvelva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 11:21:21 by grvelva           #+#    #+#             */
-/*   Updated: 2021/03/29 17:03:31 by grvelva          ###   ########.fr       */
+/*   Updated: 2021/03/30 13:02:32 by grvelva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,14 @@ void		draw_door(int size, t_sprite *spr, int n, t_all *all)
 	tmp.pos_x -= 0.5;
 	dx = tmp.pos_x - all->prms->plr.pos_x;
 	dy = tmp.pos_y - all->prms->plr.pos_y;
-	angle = atan2(-dy, dx);
+	angle =  - atan2(-dy, dx) + all->prms->plr.ang_h + M_PI / 6;
 	while (angle < 0)
 		angle += 2 * M_PI;
 	while (angle > 2 * M_PI)
 		angle -= 2 * M_PI;
 	n_start = (int)(angle * all->prms->res_h * 3 / M_PI);
 	dx = tmp.pos_x + 1 - all->prms->plr.pos_x;
-	angle = atan2(-dy, dx);
+	angle =  - atan2(-dy, dx) + all->prms->plr.ang_h + M_PI / 6;
 	while (angle < 0)
 		angle += 2 * M_PI;
 	while (angle > 2 * M_PI)
@@ -58,13 +58,13 @@ void		draw_door(int size, t_sprite *spr, int n, t_all *all)
 	while (n_start + i < n_stop)
 	{
 		angle = get_ang(all, n_start + i);
-		dist = fabs(dy * cos(angle));
+		dist = fabs(dy / sin(angle)) * cos(angle - all->prms->plr.ang_h);
 		if (dist < all->prms->dists[n_start + i])
 		{
 			h = (int)((double)all->prms->res_h / 2 / tan(M_PI / 6) / dist);
 			x_c = i * 255 / (n_stop - n_start);
 			draw_txtr_line(all, n_start + i, h | x_c << 16 | DOOR);
 		}
-		n_start++;
+		i++;
 	}
 }
