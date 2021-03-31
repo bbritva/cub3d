3c3e11e199332ac99eea228d705ff0e46f1763f6
grvelva <grvelva@student.21-school.ru>
2021-03-31 10:34:33 +0300

    занормил всё нах*й

--- render_bonus/doors.c
+++ render_bonus/doors.c
@@ -6,7 +6,7 @@
 /*   By: grvelva <grvelva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 11:21:21 by grvelva           #+#    #+#             */
-/*   Updated: 2021/03/30 16:11:27 by grvelva          ###   ########.fr       */
+/*   Updated: 2021/03/31 10:32:22 by grvelva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,52 +22,59 @@ static double	get_ang(t_all *all, int i)
 	return (ang);
 }
 
-void		draw_door(int size, t_sprite *spr, int n, t_all *all)
+static void		swap_n(int *a, int *b)
 {
-	t_sprite	tmp;
-	double		angle;
+	int tmp;
+
+	if (*a > *b)
+	{
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
+}
+
+static void		get_ns(int *n_start, int *n_stop, t_sprite door, t_all *all)
+{
 	double		dx;
 	double		dy;
-	int 		h;
-	int 		x_c;
-	int 		n_start;
-	int 		i;
-	int 		n_stop;
-	int 		n_tmp;
+	double		angle;
+
+	dx = door.pos_x - 0.5 - all->prms->plr.pos_x;
+	dy = door.pos_y - all->prms->plr.pos_y;
+	angle = -atan2(-dy, dx) + all->prms->plr.ang_h;
+	angle -= (angle > M_PI * 2) ? M_PI * 2 : 0;
+	angle += (angle < 0) ? M_PI * 2 : 0;
+	*n_start = (int)((0.5 + sin(angle)) * all->prms->res_h);
+	dx = door.pos_x + 0.5 - all->prms->plr.pos_x;
+	angle = -atan2(-dy, dx) + all->prms->plr.ang_h;
+	angle -= (angle > M_PI * 2) ? M_PI * 2 : 0;
+	angle += (angle < 0) ? M_PI * 2 : 0;
+	*n_stop = (int)((0.5 + sin(angle)) * all->prms->res_h);
+	swap_n(n_start, n_stop);
+}
+
+void			draw_door(int size, t_sprite *spr, int n, t_all *all)
+{
+	double		angle;
+	int			n_start;
+	int			i;
+	int			n_stop;
 	double		dist;
 
 	(void)size;
 	(void)n;
-	tmp = *spr;
-	tmp.pos_x -= 0.5;
-	dx = tmp.pos_x - all->prms->plr.pos_x;
-	dy = tmp.pos_y - all->prms->plr.pos_y;
-	angle =  - atan2(-dy, dx) + all->prms->plr.ang_h;
-	angle -= (angle > M_PI * 2) ? M_PI * 2 : 0;
-	angle += (angle < 0) ? M_PI * 2 : 0;
-	n_start = (int)((0.5 + sin(angle)) * all->prms->res_h);
-	dx = tmp.pos_x + 1 - all->prms->plr.pos_x;
-	angle =  - atan2(-dy, dx) + all->prms->plr.ang_h;
-	angle -= (angle > M_PI * 2) ? M_PI * 2 : 0;
-	angle += (angle < 0) ? M_PI * 2 : 0;
-	n_stop = (int)((0.5 + sin(angle)) * all->prms->res_h);
-	if (n_stop < n_start)
-	{
-		n_tmp = n_start;
-		n_start = n_stop;
-		n_stop = n_tmp;
-	}
+	get_ns(&n_start, &n_stop, *spr, all);
 	i = (n_start < 0) ? -n_start : 0;
 	while (n_start + i < n_stop && n_start + i < all->prms->res_h)
 	{
 		angle = get_ang(all, n_start + i);
-		dist = fabs(dy / sin(angle)) * cos(angle - all->prms->plr.ang_h);
+		dist = fabs((spr->pos_y - all->prms->plr.pos_y) / sin(angle)) *
+			cos(angle - all->prms->plr.ang_h);
 		if (dist < all->prms->dists[n_start + i])
-		{
-			h = (int)((double)all->prms->res_h / 2 / tan(M_PI / 6) / dist);
-			x_c = i * 255 / (n_stop - n_start);
-			draw_txtr_line(all, n_start + i, h | x_c << 16 | DOOR);
-		}
+			draw_txtr_line(all, n_start + i,
+				((int)((double)all->prms->res_h / 2 / tan(M_PI / 6) / dist))
+				| (i * 255 / (n_stop - n_start)) << 16 | DOOR);
 		i++;
 	}
 }
